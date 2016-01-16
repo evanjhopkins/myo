aeaa0046c735d76c2a90879ae05593449eddb786
popcat <moviemaker7@gmail.com>
2016-01-16 17:51:05 -0500

    reverted view controller

--- myo connect/ViewController.swift
+++ myo connect/ViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 
-
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
@@ -20,7 +19,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
