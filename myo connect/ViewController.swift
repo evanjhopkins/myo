d60135ff5ec43c41fc0769c779cfc8ed1a2b7cbb
Shirley He <she@wesleyan.edu>
2016-01-16 20:03:40 -0500

    Update ViewController.swift
    added twitter login

--- myo connect/ViewController.swift
+++ myo connect/ViewController.swift
@@ -7,11 +7,29 @@
 //
 
 import UIKit
+import TwitterKit
 
 class ViewController: UIViewController {
     
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        let logInButton = TWTRLogInButton { (session, error) in
+            if let unwrappedSession = session {
+                let alert = UIAlertController(title: "Logged In",
+                    message: "User \(unwrappedSession.userName) has logged in",
+                    preferredStyle: UIAlertControllerStyle.Alert
+                )
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+            } else {
+                NSLog("Login error: %@", error!.localizedDescription);
+            }
+        }
+        
+        logInButton.center = self.view.center
+        self.view.addSubview(logInButton)
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +37,4 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-}
 
